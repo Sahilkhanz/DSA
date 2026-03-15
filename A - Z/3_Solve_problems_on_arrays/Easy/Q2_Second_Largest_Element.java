5548ab1be41ca0fb463da4be36f53964668b4cc3
Sahilkhanz <sahilkhandeveloper@gmail.com>
2026-03-15 22:30:20 +0530

    Solve problems on arrays

--- A - Z/3_Solve_problems_on_arrays/Easy/Q2_Second_Largest_Element.java
+++ A - Z/3_Solve_problems_on_arrays/Easy/Q2_Second_Largest_Element.java
@@ -1,5 +1,5 @@
 
-public class Q3_Insertion_Sorting {
+public class Q2_Second_Largest_Element {
 
     public static void main(String[] args) {
 
--- A - Z/Learn Important Sorting Tecniques/Sorting-I/Q2_Bubble_Sort.java
+++
@@ -1,7 +0,0 @@
-
-public class Q2_Bubble_Sort {
-
-    public static void main(String[] args) {
-
-    }
-}
